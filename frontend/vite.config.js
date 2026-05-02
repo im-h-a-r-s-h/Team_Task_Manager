c054e023949db89c89b54639e1a69aad757fe1a6
Harsh Gupta <2k22.cse.2213543@gmail.com>
2026-05-02 16:01:54 +0530

    Add preview configuration with allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    allowedHosts: [
+      "accomplished-intuition-production-243d.up.railway.app"
+    ]
+  }
 })
