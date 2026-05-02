f7ddcbc5ca4660ad02d5722698c734167bf56d11
Harsh Gupta <2k22.cse.2213543@gmail.com>
2026-05-02 11:29:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const sequelize = require("./models");
+const { sequelize } = require("./models");
 
 const app = express();
 
